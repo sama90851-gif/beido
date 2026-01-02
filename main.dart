8aed6c5d3494a73fedee9c9f193100c3e6f04717
sama90851-gif <sama90851@gmail.com>
2026-01-02 22:43:50 +0300

    Refactor BeidoApp and set RegisterScreen as home
    Refactor BeidoApp to use StatelessWidget and set RegisterScreen as the home.

--- main.dart
+++ main.dart
@@ -1,54 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'screens/language_selection.dart';
-import 'screens/main_voice_hub.dart';
-import 'screens/smart_replies.dart';
-import 'screens/smart_reminders.dart';
-import 'screens/quran_screen.dart';
+import 'screens/register_screen.dart';
 import 'themes/app_theme.dart';
 
 void main() {
-  runApp(BeidoApp());
+  runApp(const BeidoApp());
 }
 
-class BeidoApp extends StatefulWidget {
-  @override
-  State<BeidoApp> createState() => _BeidoAppState();
-}
-
-class _BeidoAppState extends State<BeidoApp> {
-  Locale? _locale;
-
-  void _setLocale(Locale locale) {
-    setState(() {
-      _locale = locale;
-    });
-  }
+class BeidoApp extends StatelessWidget {
+  const BeidoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Beido',
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      locale: _locale,
-      supportedLocales: const [
-        Locale('en'),
-        Locale('ar'),
-      ],
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      initialRoute: '/',
-      routes: {
-        '/': (ctx) => LanguageSelectionScreen(onLanguageSelected: _setLocale),
-        '/main': (_) => MainVoiceHubScreen(),
-        '/smart': (_) => const SmartRepliesScreen(),
-        '/reminders': (_) => const SmartRemindersScreen(),
-        '/quran': (_) => const QuranScreen(),
-      },
+      theme: BeidoTheme.lightTheme, // نستخدم الثيم اللي سويناه
+      home: const RegisterScreen(), // نخلي أول شاشة هي التسجيل
     );
   }
 }
